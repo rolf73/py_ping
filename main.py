5a9d91951a96fbb1ea7055c524cf4de414d0fc03
rolf <rolf.nemholt@gmail.com>
2022-01-13 22:51:33 +0100

    create bar chart

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from pythonping import ping
+import matplotlib.pyplot as plt
 
-
-def run():
-    ip_list = ['127.0.0.1']
+def ping_list(ip_list):
+    
+    result_list = []
+    rtt_avg_list = []
     for i in ip_list:
-        ping(i, count=5, verbose=True)
-    return
+        result = ping(i, count=1, verbose=False)
+        rtt_avg_list.append(result.rtt_avg)
+        print("host: {0} rtt_avg: {1}".format(i, result.rtt_avg))
+
+    #plt.plot(rtt_avg_list)
+    plt.bar(ip_list, rtt_avg_list)
+    plt.ylabel('rtt_avg')
+    plt.show()
+
+    return 
 
 
 if __name__ == '__main__':
-    run()
+    ip_list = ['127.0.0.1', 'rpi4-20211030', 'OnePlus-7T', 'dr.dk', 'Chromecast', 'LGwebOSTV', '192.168.1.114']
+    ping_list(ip_list)
